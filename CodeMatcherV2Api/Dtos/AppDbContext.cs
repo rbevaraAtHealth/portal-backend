b8644c7566deb1d6d79cdf9d42b806f8bb93439f
Keerthana Gobburi <keerthana.gobburi@healthstream.com>
2023-07-05 18:20:31 +0530

    Created AppDbContext and 2 tables were created and checked the same in SQL server.

---
+++ CodeMatcherV2Api/Dtos/AppDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CodeMatcherV2Api.Dtos
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LookupTypeDto> LookupTypes { get; set; }
+        public DbSet<LookupDto> Lookups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<LookupTypeDto>().HasData(
+                new LookupTypeDto()
+                {
+                    LookupTypeId = 1,
+                    LookupTypeKey = "Segment",
+                    LookupTypeDescription = "This lookup defines the segments while creating the jobs.",
+                });
+
+            modelBuilder.Entity<LookupDto>().HasData(
+                new LookupDto()
+                {
+                    Id = 1,
+                    LookupTypeId = 1,
+                    Name = "School",
+                });
+
+            modelBuilder.Entity<LookupDto>().HasData(
+                new LookupDto()
+                {
+                    Id = 2,
+                    LookupTypeId = 1,
+                    Name = "Insurance",
+                });
+
+            modelBuilder.Entity<LookupDto>().HasData(
+                new LookupDto()
+                {
+                    Id = 3,
+                    LookupTypeId = 1,
+                    Name = "State License",
+                });
+
+            modelBuilder.Entity<LookupDto>().HasData(
+                new LookupDto()
+                {
+                    Id = 4,
+                    LookupTypeId = 1,
+                    Name = "Hospital",
+                });
+        }
+    }
+}
---
+++ CodeMatcherV2Api/Dtos/LookupTypeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeMatcherV2Api.Dtos
+{
+    public class LookupTypeDto
+    {
+        [Key]
+        public int LookupTypeId { get; set; }
+        public string LookupTypeKey { get; set; }
+        public string LookupTypeDescription { get; set; }
+    }
+}
