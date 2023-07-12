f063d2977f2255305b9c420430ae456b448cffff
Uday Kiran <uday@olobby.com>
2023-07-12 17:25:12 +0530

    Updated cors command

--- CodeMatcherV2Api/Startup.cs
+++ CodeMatcherV2Api/Startup.cs
@@ -27,7 +27,10 @@ namespace CodeMatcherV2Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
             services.AddDbContext<AppDbContext>(options =>
                                         options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
             services.AddControllers();
