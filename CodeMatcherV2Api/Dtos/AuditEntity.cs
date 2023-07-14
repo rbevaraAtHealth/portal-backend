6253f7ea808146baae928d503c72c1e7dd51dad6
Keerthana Gobburi <keerthana.gobburi@healthstream.com>
2023-07-14 17:39:43 +0530

    Added CodeGeneration and Embeddings Model and DTO's. Base data for Lookup and LookupTypes are also added using sql scripts.

---
+++ CodeMatcherV2Api/Dtos/AuditEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace CodeMatcherV2Api.Dtos
+{
+    public abstract class AuditEntity
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string CreatedBy { get; set; }
+
+        private DateTime created;
+
+        public virtual DateTime? CreatedTime
+        { 
+            get { return created; }
+            private set { created = DateTime.Now; } 
+        }
+
+        public virtual string ModifiedBy { get; set; }
+
+        private DateTime modified;
+
+        public virtual DateTime? ModifiedTime
+        {
+            get { return modified; }
+            private set { modified = DateTime.Now; }
+        }
+
+        public virtual bool? IsDeleted { get; set; }
+    }
+}
--- CodeMatcherV2Api/Migrations/20230714115814_InitialModel.Designer.cs
+++ CodeMatcherV2Api/Migrations/20230714115814_InitialModel.Designer.cs
@@ -4,6 +4,7 @@ using CodeMatcherV2Api.Dtos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodeMatcherV2Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230714115814_InitialModel")]
+    partial class InitialModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
