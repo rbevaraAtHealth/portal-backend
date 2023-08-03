37df1ade46f00704bd886f65f2c17546a9638b5b
Keerthana Gobburi <keerthana.gobburi@healthstream.com>
2023-08-03 16:59:39 +0530

    Scheduler Process Api end point created

---
+++ CodeMatcherV2Api/Controllers/SchedulerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using CodeMatcher.Api.V2.BusinessLayer.Interfaces;
+
+namespace CodeMatcher.Api.V2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulerController : ControllerBase
+    {
+        public readonly IScheduler _scheduler;
+        public SchedulerController(IScheduler scheduler)
+        {
+            _scheduler = scheduler;
+        }
+        [HttpGet, Route("GetSchedulerRecords")]
+        public async Task<IActionResult> GetAllSchedulers()
+        {
+            try
+            {
+                var records = await _scheduler.GetAllSchedulersAsync();
+                return Ok(records);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
