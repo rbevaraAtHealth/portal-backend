f01bf7a5fe1a2f2b065fc24116880fd7c0fb6faf
Uday Kiran <uday@olobby.com>
2023-08-16 13:58:18 +0530

    Updated appsettings json files

--- CodeMatcherV2Api/Startup.cs
+++ CodeMatcherV2Api/Startup.cs
@@ -26,9 +26,14 @@ namespace CodeMatcherV2Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName ?? "Development"}.json", optional: true)
+            .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
