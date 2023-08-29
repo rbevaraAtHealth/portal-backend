ca0fc2bba1f570c5342f12b84c353d596a28bdbe
Uday Kiran <uday@olobby.com>
2023-08-29 13:12:57 +0530

    Changed datatype

--- CodeMatcherV2Api/Models/SummaryModel/GenericSummaryViewModel.cs
+++ CodeMatcherV2Api/Models/SummaryModel/GenericSummaryViewModel.cs
@@ -13,8 +13,8 @@ namespace CodeMatcher.Api.V2.Models.SummaryModel
         public string Segment { get; set; }
         public string CodeMappingType { get; set; }
         public string Threshold { get; set; }
-        public int NoOfRecords { get; set; }
-        public int NoofMatches { get; set; }
+        public int? NoOfRecords { get; set; }
+        public int? NoofMatches { get; set; }
         public string RunBy { get; set; }
         public string RunType { get; set; }
         public string Status { get; set; }
