8d3d6601b7585705c4c24ebd5c26ae2a3f5267a4
Uday Kiran <uday@olobby.com>
2023-08-31 15:41:13 +0530

    Added logs

--- SchedulerProcessor/SchedulerProcessor.cs
+++ SchedulerProcessor/SchedulerProcessor.cs
@@ -52,78 +52,67 @@ namespace SchedulerProcessor
                 if (response.IsSuccessStatusCode)
                 {
                     var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                    var data = JsonConvert.DeserializeObject(result);
-                    //List<SchedulerModel> schedulerModels1 = new List<SchedulerModel>((IEnumerable<SchedulerModel>)result.ToList());
-                    JObject obj = JObject.Parse(result);
-                    JToken token = obj["model"];
-                    List<SchedulerModel> list = new List<SchedulerModel>();
-                    list = token;
+                    var data = JsonConvert.DeserializeObject<ResponseViewModel>(result);
                     if (data != null)
                     {
-                        foreach (var item in token)
+                        log.LogInformation($"All schedules Data: {data.Model}");
+                        var list = JsonConvert.DeserializeObject<List<SchedulerModel>>(data.Model.ToString());
+                        
+                        foreach (var details in list)
                         {
-                            List<SchedulerModel> schedulerModels = new List<SchedulerModel>();
-                            foreach (var item2 in schedulerModels)
+                            var schedule = CrontabSchedule.TryParse(details.CronExpression).GetNextOccurrence(curExecutionDate);
+                            if (schedule >= curExecutionDate && schedule <= nextRunSchedule)
                             {
-                                
+                                log.LogInformation($"Call Job API of CLientId: {details.ClientId}");
+                                if (details.CronExpression != null)
+                                {
+                                    _httpClient.DefaultRequestHeaders.Add("ClientID", details.ClientId);
+                                    if (details.CodeMapping.ToLower() == "code generation")
+                                    {
+                                        CgTriggerRunModel models = new CgTriggerRunModel();
+                                        models.Segment = details.Segment;
+                                        models.Threshold = details.Threshold;
+                                        var requestContent = JsonConvert.SerializeObject(models);
+                                        var content = new StringContent(requestContent, System.Text.Encoding.UTF8, "application/json");
+
+                                        var cgTriggeredUrl = _httpClient.BaseAddress + "TriggeredRun/CodeGenerationTriggerRun";
+                                        var cgTriggeredResponse = await _httpClient.PostAsync(cgTriggeredUrl, content);
+                                        var cgData = cgTriggeredResponse.Content.ReadAsAsync<List<CgTriggerRunModel>>();
+                                    }
+                                    else if (details.CodeMapping.ToLower() == "weekly embedding")
+                                    {
+                                        WeeklyEmbedTriggeredRunModel models = new WeeklyEmbedTriggeredRunModel();
+                                        models.Segment = details.Segment;
+                                        var requestContent = JsonConvert.SerializeObject(models);
+                                        var content = new StringContent(requestContent, System.Text.Encoding.UTF8, "application/json");
+
+                                        var cgTriggeredUrl = _httpClient.BaseAddress + "TriggeredRun/WeeklyEmbeddingTriggerRun";
+                                        var cgTriggeredResponse = await _httpClient.PostAsync(cgTriggeredUrl, content);
+                                        var cgData = cgTriggeredResponse.Content.ReadAsAsync<List<WeeklyEmbedTriggeredRunModel>>();
+                                    }
+                                    else if (details.CodeMapping.ToLower() == "monthly embedding")
+                                    {
+                                        MonthlyEmbedTriggeredRunModel models = new MonthlyEmbedTriggeredRunModel();
+                                        models.Segment = details.Segment;
+                                        var requestContent = JsonConvert.SerializeObject(models);
+                                        var content = new StringContent(requestContent, System.Text.Encoding.UTF8, "application/json");
+
+                                        var cgTriggeredUrl = _httpClient.BaseAddress + "TriggeredRun/MonthlyEmbeddingTriggerRun";
+                                        var cgTriggeredResponse = await _httpClient.PostAsync(cgTriggeredUrl, content);
+                                        var cgData = cgTriggeredResponse.Content.ReadAsAsync<List<MonthlyEmbedTriggeredRunModel>>();
+                                    }
+                                    else
+                                    {
+                                        log.LogInformation($"");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                log.LogInformation($"Next schedule for clientId - {details.ClientId} is at - {schedule}");
                             }
 
                         }
-                        //foreach (var details in SchedulerModel(data.Model))
-                        //{
-                        //    var schedule = CrontabSchedule.TryParse(details.CronExpression).GetNextOccurrence(curExecutionDate);
-                        //    if (schedule >= curExecutionDate && schedule <= nextRunSchedule)
-                        //    {
-                        //        log.LogInformation($"Call Job API of CLientId: {details.ClientId}");
-                        //        if (details.CronExpression != null)
-                        //        {
-                        //            _httpClient.DefaultRequestHeaders.Add("ClientID", details.ClientId);
-                        //            if (details.CodeMapping.ToLower() == "code generation")
-                        //            {
-                        //                CgTriggerRunModel models = new CgTriggerRunModel();
-                        //                models.Segment = details.Segment;
-                        //                models.Threshold = details.Threshold;
-                        //                var requestContent = JsonConvert.SerializeObject(models);
-                        //                var content = new StringContent(requestContent, System.Text.Encoding.UTF8, "application/json");
-
-                        //                var cgTriggeredUrl = _httpClient.BaseAddress + "TriggeredRun/CodeGenerationTriggerRun";
-                        //                var cgTriggeredResponse = await _httpClient.PostAsync(cgTriggeredUrl, content);
-                        //                var cgData = cgTriggeredResponse.Content.ReadAsAsync<List<CgTriggerRunModel>>();
-                        //            }
-                        //            else if (details.CodeMapping.ToLower() == "weekly embedding")
-                        //            {
-                        //                WeeklyEmbedTriggeredRunModel models = new WeeklyEmbedTriggeredRunModel();
-                        //                models.Segment = details.Segment;
-                        //                var requestContent = JsonConvert.SerializeObject(models);
-                        //                var content = new StringContent(requestContent, System.Text.Encoding.UTF8, "application/json");
-
-                        //                var cgTriggeredUrl = _httpClient.BaseAddress + "TriggeredRun/WeeklyEmbeddingTriggerRun";
-                        //                var cgTriggeredResponse = await _httpClient.PostAsync(cgTriggeredUrl, content);
-                        //                var cgData = cgTriggeredResponse.Content.ReadAsAsync<List<WeeklyEmbedTriggeredRunModel>>();
-                        //            }
-                        //            else if (details.CodeMapping.ToLower() == "monthly embedding")
-                        //            {
-                        //                MonthlyEmbedTriggeredRunModel models = new MonthlyEmbedTriggeredRunModel();
-                        //                models.Segment = details.Segment;
-                        //                var requestContent = JsonConvert.SerializeObject(models);
-                        //                var content = new StringContent(requestContent, System.Text.Encoding.UTF8, "application/json");
-
-                        //                var cgTriggeredUrl = _httpClient.BaseAddress + "TriggeredRun/MonthlyEmbeddingTriggerRun";
-                        //                var cgTriggeredResponse = await _httpClient.PostAsync(cgTriggeredUrl, content);
-                        //                var cgData = cgTriggeredResponse.Content.ReadAsAsync<List<MonthlyEmbedTriggeredRunModel>>();
-                        //            }
-                        //            else
-                        //            {
-                        //                log.LogInformation($"");
-                        //            }
-                        //        }
-                        //    }
-                        //    else
-                        //    {
-                        //        log.LogInformation($"Next schedule for clientId - {details.ClientId} is at - {schedule}");
-                        //    }
-
-                        //}
                     }
                     else
                         log.LogInformation("Data cannot be null.");
