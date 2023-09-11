6f2d76ec4bad581ab5d759356193459459a9eb31
Uday Kiran <uday@olobby.com>
2023-09-12 01:45:18 +0530

    Fixed time format

--- CodeMatcherV2Api/MapperConfig.cs
+++ CodeMatcherV2Api/MapperConfig.cs
@@ -22,13 +22,13 @@ namespace CodeMatcherV2Api
                 config.CreateMap<LookupModel, LookupDto>().ReverseMap();
                 config.CreateMap<LookupTypeDto, LookupTypeModel>().ReverseMap();
                 config.CreateMap<CodeGenerationSummaryModel, CodeGenerationSummaryDto>()
-                 .ForMember(x => x.Date, y => y.MapFrom(z => DateTime.ParseExact(z.Date, "dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture)))
+                 .ForMember(x => x.Date, y => y.MapFrom(z => DateTime.ParseExact(z.Date, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture)))
                  .ReverseMap();
                 config.CreateMap<MonthlyEmbedSummaryModel, MonthlyEmbeddingsSummaryDto>()
-                .ForMember(x => x.Date, y => y.MapFrom(z => DateTime.ParseExact(z.Date, "dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture)))
+                .ForMember(x => x.Date, y => y.MapFrom(z => DateTime.ParseExact(z.Date, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture)))
                  .ReverseMap();
                 config.CreateMap< WeeklyEmbedSummaryModel, WeeklyEmbeddingsSummaryDto>()
-                .ForMember(x => x.Date, y => y.MapFrom(z => DateTime.ParseExact(z.Date, "dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture)))
+                .ForMember(x => x.Date, y => y.MapFrom(z => DateTime.ParseExact(z.Date, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture)))
                  .ReverseMap();
                 config.CreateMap<CodeMappingRequestDto, CgTriggeredRunReqModel>().ReverseMap();
                 config.CreateMap<LogTableModel, LogTableDto>().ReverseMap();
